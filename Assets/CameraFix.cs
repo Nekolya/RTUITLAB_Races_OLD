fb657419b0a6333c6c944624fc9d5f1f406f3415
Olya Bykovskaya <olya75825@gmail.com>
2020-04-05 23:54:41 +0300

    Initial commit

---
+++ Assets/CameraFix.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFix : MonoBehaviour
+{
+    public GameObject car;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, car.transform.eulerAngles.y, 0);
+    }
+}
